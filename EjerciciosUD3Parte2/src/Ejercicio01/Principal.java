56e8bc0dc45856fa2efc9fdffd1831975ee05fb3
villamacri <villalba.macri24@triana.salesianos.edu>
2023-11-28 21:24:13 +0100

    los gritos del sergio

--- EjerciciosUD3Parte2/src/Ejercicio01/Principal.java
+++ EjerciciosUD3Parte2/src/Ejercicio01/Principal.java
@@ -6,9 +6,92 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stu
-		
 		Scanner  sc=new Scanner(System.in);
-
+		
+		int tam=100;
+		Producto [] lista= new Producto[tam];
+		int opcion = 0;
+		int opcionFragil = 0;
+		double precioFabric = 0;
+		double pvp = 0;
+		double precioTrans = 0;
+		String codigo;
+		String nombreProducto;
+		String aux;
+		
+		Tienda t=new Tienda(lista, 0);
+		
+		System.out.println("************************************");
+		System.out.println();
+		System.out.println("Bienvenido a la tienda de la esquina");
+		System.out.println();
+		System.out.println("************************************");
+		
+		do {
+			System.out.println();
+			System.out.println("""
+								¿Que desea hacer?
+								
+								1. Para mostrar lista
+								2. Para añadir un producto
+								3. Para calcular inversión
+								4. Para calcular PVP
+								5. Para calcular ganancias
+								""");
+			aux=sc.nextLine();
+			opcion=Integer.parseInt(aux);
+			
+			switch(opcion) {
+			
+			case 1:
+				t.listarProductos();
+				break;
+				
+			case 2:
+				
+				System.out.println("Diga precio fabrica");
+				aux=sc.nextLine();
+				precioFabric=Integer.parseInt(aux);
+				
+				System.out.println("Diga codigo del producto");
+				codigo=sc.nextLine();
+				
+				System.out.println("Diga nombre del producto");
+				nombreProducto=sc.nextLine();
+				
+				System.out.println("¿El producto es frágil?");
+				do {
+					System.out.println("""
+										opcion 1: Si
+										opcion 2: No
+										""");
+					aux=sc.nextLine();
+					opcionFragil=Integer.parseInt(aux);
+					
+					
+				}while(opcionFragil!=1 && opcionFragil!=2);
+				
+				t.agregar(new Producto(precioFabric, codigo, nombreProducto, t.comprobarFragil(opcionFragil)));
+				break;
+			
+			case 3:
+				System.out.println(t.calcularInversion());
+				break;
+				
+			case 4:
+				System.out.println(t.calcularTotalPVP(pvp, precioFabric));
+				break;
+				
+			case 5:
+				System.out.println(t.calcularGanancias(pvp, precioTrans));
+				
+			}
+			
+		
+			
+			
+		}while(opcion!=0);
+		
 		
 		
 	}
---
+++ PruebaDebugger/src/EjercicioDebugger/PruebaDebugger.java
@@ -0,0 +1,13 @@
+package EjercicioDebugger;
+
+public class PruebaDebugger {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ClaseParaDepurar clase = new ClaseParaDepurar();
+    	double valor = clase.echaCuentas();
+    	System.out.println("Sale "+valor);
+
+	}
+
+}
