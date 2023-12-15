06fa706b0088e9ae5bdcc88b0a8e97ce5b6e482a
Cristian Daniel Villalba Marín <villalba.macri24@triana.salesianos.edu>
2023-12-15 12:42:50 +0100

    Actualizacion repaso

--- RepasoTema3/src/Ejercicio1/Principal.java
+++ RepasoTema3/src/Ejercicio1/Principal.java
@@ -1,9 +1,12 @@
 package Ejercicio1;
 
+import java.util.Scanner;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
 		
 		//int a=8;
 		//Producto p=new Producto(null, a, a, false); //No es buena idea crear un objeto vacío
@@ -16,8 +19,94 @@ public class Principal {
 		//Secciones 3 distintas
 		//
 		
-		Producto p=new Producto("Lechuga", 1.20, 1, true);
+		double precioFabrica = 0;
+		int opcion=0, seccion = 0, opcionEnVenta = 0, tam = 0, tam2 = 10;
+		boolean enVenta = false;
+		String aux, nombre = null;
+		Producto p;
+		Producto p1=new Producto("Pantalla",250 , 3, true);
+		Producto p2=new Producto("Pantalón", 30, 2, true);
+		Producto [] lista= new Producto[tam2];
+		Tienda t=new Tienda(lista, tam, 3);
 		
+		t.agregar(p1);
+		t.agregar(p2);
+		
+		
+		
+		//System.out.println(precioFabrica);
+		//Arriba se trabaja con una variable de la clase main  y abajo von el atributo del objeto
+		//System.out.println(p.getPrecioFabrica());
+		
+		//p.setPrecioFabrica(precioFabrica);
+		//System.out.println(p);
+		
+		do {
+			System.out.println("""
+								Diga que desea hacer
+								
+								0. Salir
+								1. Agregar producto
+								2. Mostrar lista
+								3. Mostrar cualquier lista
+								""");
+			aux=sc.nextLine();
+			opcion=Integer.parseInt(aux);
+			
+			switch(opcion) {
+			
+			case 0:
+				System.out.println("Saliendo...");
+				break;
+				
+			case 1:
+				System.out.println("Diga nombre del producto");
+				nombre=sc.nextLine();
+				
+				System.out.println("Diga precio fabrica del producto");
+				aux=sc.nextLine();
+				precioFabrica=Double.parseDouble(aux);
+				
+				System.out.println("""
+									Indique seccion
+									1. Alimentación
+									2. Ropa
+									3. Electrónica
+									""");
+				aux=sc.nextLine();
+				seccion=Integer.parseInt(aux);
+				
+				System.out.println("Diga si está en venta: 1 en venta, 2 no esta en venta");
+				aux=sc.nextLine();
+				opcionEnVenta=Integer.parseInt(aux);
+				
+				while(opcionEnVenta!=1&&opcionEnVenta!=2) {
+				
+				if(opcionEnVenta==1) {
+					enVenta=true;
+				}else {
+					enVenta=false;
+				}
+			
+				}
+				
+				p=new Producto(nombre, precioFabrica, seccion, enVenta);
+				t.agregar(p);
+				break;
+				
+			case 2:
+				t.mostrarProductos();
+				break;
+				
+			case 3:
+				System.out.println("Diga sección a ver");
+				aux=sc.nextLine();
+				seccion=Integer.parseInt(aux);
+				
+				t.mostrarListaV2(t.buscarBySeccion(seccion));
+			}	
+			
+		}while(opcion!=0);
 	}
 
 }
--- RepasoTema3/src/Ejercicio1/Producto.java
+++ RepasoTema3/src/Ejercicio1/Producto.java
@@ -2,7 +2,9 @@ package Ejercicio1;
 
 public class Producto {
 	
-
+//Los getter and setters sirven para acceder a los datos
+//de los atributos desde otra clase
+//
 
 	private String nombre;
 	private double precioFabrica;
@@ -16,6 +18,45 @@ public class Producto {
 		this.seccion = seccion;
 		this.enVenta = enVenta;
 	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public double getPrecioFabrica() {
+		return precioFabrica;
+	}
+
+	public void setPrecioFabrica(double precioFabrica) {
+		this.precioFabrica = precioFabrica;
+	}
+
+	public int getSeccion() {
+		return seccion;
+	}
+
+	public void setSeccion(int seccion) {
+		this.seccion = seccion;
+	}
+
+	public boolean isEnVenta() {
+		return enVenta;
+	}
+
+	public void setEnVenta(boolean enVenta) {
+		this.enVenta = enVenta;
+	}
+
+	@Override
+	public String toString() {
+		return "Producto [nombre=" + nombre + ", precioFabrica=" + precioFabrica + ", seccion=" + seccion + ", enVenta="
+				+ enVenta + "]";
+	}
+	
 	
 	
 	
