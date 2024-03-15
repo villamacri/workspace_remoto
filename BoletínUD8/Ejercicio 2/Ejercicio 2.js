7fdf1a7535039e9ed29906f33a233a1264ed0ca0
Cristian Daniel Villalba Marín <villalba.macri24@triana.salesianos.edu>
2024-03-15 14:27:05 +0100

    15/03/2024

---
+++ BoletínUD8/Ejercicio 2/Ejercicio 2.js
@@ -0,0 +1,3 @@
+let mensaje="Mensaje de prueba :D";
+    alert(mensaje);
+
---
+++ BoletínUD8/Ejercicio 3/Ejercicio 3.js
@@ -0,0 +1,2 @@
+let meses = [" Enero", " Febrero", " Marzo", " Abril", " Mayo", " Junio", " Julio", " Agosto", " Septiembre", " Octubre", " Noviembre", " Diciembre"];
+alert(meses);
