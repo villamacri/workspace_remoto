b376f28ac57e1d27abdd68b1ca768b90fb3d3563
Cristian Daniel Villalba Marín <villalba.macri24@triana.salesianos.edu>
2024-03-19 13:31:04 +0100

    19/03/2024

---
+++ EjemplosUD6/src/EjemploFecha/FechaInvalida.java
@@ -0,0 +1,25 @@
+package EjemploFecha;
+
+public class FechaInvalida extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public FechaInvalida() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public FechaInvalida(String message) {
+		super("Fecha no válida");
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+	
+	
+}
---
+++ EjemplosUD6/src/EjemploFecha/GestionReserva.java
@@ -0,0 +1,12 @@
+package EjemploFecha;
+
+import java.time.LocalDate;
+
+public class GestionReserva {
+	
+	public void hacerReserva() throws FechaInvalida{
+		LocalDate hoy= LocalDate.now();
+		
+	}
+
+}
